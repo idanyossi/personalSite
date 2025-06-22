13b6bcddc981480c0a1f18797ef44879a9b2a10f
idanyossi <146638614+idanyossi@users.noreply.github.com>
2025-06-22 13:05:23 +0300

    added expanded cards to the project section

--- personal-site/src/components/personalprojects/components/project-card/project-card.jsx
+++ personal-site/src/components/personalprojects/components/project-card/project-card.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import "./project-card.css";
 
 export default function ProjectCard({ project }) {
+  const [Expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!Expanded);
+  };
+  const handleClose = () => {
+    setExpanded(false);
+  };
   return (
-    <div className="project-card">
-      <div className="icon-wrapper">
-        <img src={project.icon} alt="" className="icon" />
+    <>
+      <div className="project-card" onClick={handleClick}>
+        <div className="icon-wrapper">
+          <img src={project.icon} alt="" className="icon" />
+        </div>
+        <h3>{project.title}</h3>
       </div>
-      <h3>{project.title}</h3>
-    </div>
+      {Expanded && (
+        <div className="expanded-card" onClick={handleClose}>
+          <div className="expanded-card-content">
+            <button className="close">x</button>
+            <div>
+              <img src={project.icon} alt="" className="icon" />
+            </div>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- personal-site/src/components/personalprojects/projects.jsx
+++ personal-site/src/components/personalprojects/projects.jsx
@@ -10,12 +10,16 @@ export default function Projects() {
           project={{
             title: "Twine Mobile App",
             icon: "./twine.png",
+            description:
+              "a mobile app for job searching, built with React Native and using mongoDB and FastAPI for the backend. Meant to make job searching easier and more streamlined with less bloat",
           }}
         />
         <ProjectCard
           project={{
             title: "Personal Portfolio",
             icon: "./goofyphoto.jpg",
+            description:
+              "a personal portfolio website built with React, showcasing my skills and projects. It includes a responsive design and smooth navigation for an optimal user experience.",
           }}
         />
       </div>
