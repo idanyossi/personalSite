29bc1a687c70e74449e6fc7eb8293a7e6c1e0717
idanyossi <146638614+idanyossi@users.noreply.github.com>
2025-06-22 12:13:53 +0300

    added active scrolling feature to the navbar and active marking of the current section

--- personal-site/src/App.jsx
+++ personal-site/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import About from "./components/about/about.jsx";
 import Navbar from "./components/navbar/navbar.jsx";
 import Projects from "./components/personalprojects/projects.jsx";
@@ -8,6 +8,26 @@ function App() {
   const aboutRef = useRef(null);
   const projectsRef = useRef(null);
   const [activeSection, setActiveSection] = useState("about");
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.8,
+    };
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.getAttribute("data-section"));
+        }
+      });
+    }, option);
+
+    const sections = [aboutRef.current, projectsRef.current];
+    sections.forEach((sec) => sec && observer.observe(sec));
+
+    return () => sections.forEach((sec) => sec && observer.unobserve(sec));
+  }, []);
 
   const handleNavClick = (section) => {
     setActiveSection(section);
@@ -20,10 +40,10 @@ function App() {
   return (
     <div className="app-lineup">
       <Navbar activeSection={activeSection} onNavClick={handleNavClick} />
-      <section ref={aboutRef}>
+      <section ref={aboutRef} data-section="about">
         <About />
       </section>
-      <section ref={projectsRef}>
+      <section ref={projectsRef} data-section="projects">
         <Projects />
       </section>
     </div>
--- personal-site/src/components/navbar/navbar.jsx
+++ personal-site/src/components/navbar/navbar.jsx
@@ -1,22 +1,20 @@
 import "./navbar.css";
 
-export default function Navbar({ activeSelection, onNavClick }) {
+export default function Navbar({ activeSection, onNavClick }) {
   return (
     <header className="navbar">
       <nav className="navbar-container">
         <div className="Name"> Idan Yossifov </div>
         <ul className="navbar-ul">
           <li
-            className={`navbar-li${
-              activeSelection === "About" ? " active" : ""
-            }`}
+            className={`navbar-li${activeSection === "about" ? " active" : ""}`}
             onClick={() => onNavClick("about")}
           >
             About
           </li>
           <li
             className={`navbar-li${
-              activeSelection === "Projects" ? " active" : ""
+              activeSection === "projects" ? " active" : ""
             }`}
             onClick={() => onNavClick("projects")}
           >
